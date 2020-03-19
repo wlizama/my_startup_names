eadff05b20c527e07da38cb59fbe5349a3f323ca
Wilder Lizama Garcia <wilderlizama@gmail.com>
2020-03-18 23:43:45 -0500

    Favorites list

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,33 @@ class RandomWordsState extends State<RandomWords> {
 	final _biggerFont = const TextStyle(fontSize: 18.0);
 
 	void _pushSaved () {
-		
+		Navigator.of(context).push(
+			MaterialPageRoute<void>(
+				builder: (BuildContext context) {
+					final tiles = _saved.map(
+						(WordPair pair) {
+							return ListTile(
+								title: Text(
+									pair.asPascalCase,
+									style: _biggerFont,
+								),
+							);
+						}
+					);
+					final divided = ListTile.divideTiles(
+						context: context,
+						tiles: tiles
+					).toList();
+
+					return Scaffold(
+						appBar: AppBar(
+							title: Text("Favorites names"),
+						),
+						body: ListView(children: divided),
+					);
+				}
+			)
+		);
 	}
 
 	@override
@@ -30,7 +56,7 @@ class RandomWordsState extends State<RandomWords> {
 			appBar: AppBar(
 				title: Text("My Startup names"),
 				actions: <Widget>[
-					IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+					IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
 				],
 			),
 			body: _buildsuggestions(),
