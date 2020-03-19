b3b85865d4ce60c7a27d692511515d2994e8f762
Wilder Lizama Garcia <wilderlizama@gmail.com>
2020-03-18 23:31:35 -0500

    Favorites

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,48 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
 	final _suggestions = <WordPair>[];
+	final _saved = Set<WordPair>();
 	final _biggerFont = const TextStyle(fontSize: 18.0);
+
+	void _pushSaved () {
+		
+	}
 
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
 			appBar: AppBar(
 				title: Text("My Startup names"),
+				actions: <Widget>[
+					IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+				],
 			),
 			body: _buildsuggestions(),
 		);
 	}
 
 	Widget _buildRow(WordPair pair) {
+		final alreadySaved = _saved.contains(pair);
+
 		return ListTile(
 			title: Text(
 				pair.asPascalCase,
 				style: _biggerFont,
 			),
+			trailing: Icon(
+				alreadySaved ? Icons.favorite : Icons.favorite_border,
+				color: alreadySaved ? Colors.red : null,
+			),
+			onTap: () {
+				setState(() {
+				  if (alreadySaved) {
+					  _saved.remove(pair);
+				  }
+				  else {
+					  _saved.add(pair);
+				  }
+				});
+			},
 		);
 	}
 
